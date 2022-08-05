a8b2bdf6a463f0702c8ed8a65f879375ccecaaee
ISH <70657630+elementISH@users.noreply.github.com>
2022-08-05 14:39:15 +0200

    update

--- frontend/src/components/home/content/Status.jsx
+++ frontend/src/components/home/content/Status.jsx
@@ -8,33 +8,6 @@ export default function Status(props) {
     <>
       <div className="status__container">
         <div className="status container">
-          {/* <div className="state">
-            <p className="number">6</p>
-            <p className="indicator">rooms</p>
-            <FontAwesomeIcon
-              icon="arrow-right-to-bracket"
-              className="react-icon"
-              style={{ fontSize: "22px" }}
-            />
-          </div>
-          <div className="state">
-            <p className="number">275</p>
-            <p className="indicator">locks</p>
-            <FontAwesomeIcon
-              icon="lock"
-              className="react-icon"
-              style={{ fontSize: "22px" }}
-            />
-          </div>
-          <div className="state">
-            <p className="number">35132</p>
-            <p className="indicator">players</p>
-            <FontAwesomeIcon
-              icon="users"
-              className="react-icon"
-              style={{ fontSize: "22px" }}
-            />
-          </div> */}
           {data &&
             data?.map((state) => (
               <div className="state">
