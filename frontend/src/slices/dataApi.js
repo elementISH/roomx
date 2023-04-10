d026efb4466e2f5fdc985fb086cb35c55ac58ad4
ISH <70657630+elementISH@users.noreply.github.com>
2023-04-10 23:04:58 +0200

    production upload

--- frontend/src/slices/dataApi.js
+++ frontend/src/slices/dataApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const dataApi = createApi({
   reducerPath: "dataApi",
-  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:8080/" }),
+  baseQuery: fetchBaseQuery({ baseUrl: "https://roomx-provider.vercel.app/" }),
   endpoints: (builder) => ({
     getData: builder.query({
       query: (params) => `${params ? params : "rooms"}`,
